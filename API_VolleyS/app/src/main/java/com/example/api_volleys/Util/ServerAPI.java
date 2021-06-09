12352c03def1a82a112f55e5072590799a02543a
SellyRizky <Ratihcantik785@gmail.com>
2021-06-09 20:01:44 +0700

    android_API

---
+++ API_VolleyS/app/src/main/java/com/example/api_volleys/Util/ServerAPI.java
@@ -0,0 +1,8 @@
+package com.example.api_volleys.Util;
+
+public class ServerAPI {
+    public static final String URL_DATA = "C:\\xampp\\htdocs\\C_E31191855_Selly-Rizky_Web";
+    public static final String URL_INSERT = "C:\\xampp\\htdocs\\C_E31191855_Selly-Rizky_Web";
+    public static final String URL_DELETE = "C:\\xampp\\htdocs\\C_E31191855_Selly-Rizky_Web";
+    public static final String URL_UPDATE = "C:\\xampp\\htdocs\\C_E31191855_Selly-Rizky_Web";
+}
